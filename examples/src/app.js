212d08cd8f93e4e8fbb3b6790b3f1bdd9c74dd1f
Joss Mackison <joss@thinkmill.com.au>
2016-04-05 14:05:08 +1000

    linter fixes

--- examples/src/app.js
+++ examples/src/app.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import Lightbox from 'react-images';
 import Gallery from './components/Gallery';
 
-function capitalizeFirstLetter(str) {
+function capitalizeFirstLetter (str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
@@ -17,18 +17,19 @@ const IMAGE_MAP = IMAGE_NAMES.map(img => ({
 		`./images/500-${img}.jpg 500w`,
 		`./images/320-${img}.jpg 320w`,
 	],
-	caption: capitalizeFirstLetter(img)
+	caption: capitalizeFirstLetter(img),
 }));
 const IMAGES_PRELOAD = IMAGE_MAP.map(img => {
 	return <img key={img.caption} src={`./images/1024-${img}.jpg`} />;
 });
 
-render (
-	<div>
+render(
+	<section id="examples" className="section-examples">
+		<h2>Examples</h2>
 		<p style={{ marginBottom: 40 }}>Use your keyboard to navigate <kbd>left</kbd> <kbd>right</kbd> <kbd>esc</kbd> &mdash; Also, try resizing your browser window.</p>
 		<Gallery images={IMAGE_MAP} />
 		<p>Images courtesy of <a href="http://gratisography.com/" target="_blank">Gratisography</a></p>
-		<div style={{ display: 'none' }}>{IMAGES_PRELOAD}</div>
-	</div>,
+		{/*<div style={{ display: 'none' }}>{IMAGES_PRELOAD}</div>*/}
+	</section>,
 	document.getElementById('example')
 );
